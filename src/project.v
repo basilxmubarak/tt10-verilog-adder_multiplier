a91ce12419cdb7b2c67c6313b59268ef1a63f398
Basil Mubarak <basilmubarak008@gmail.com>
2025-03-19 00:19:23 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -38,8 +38,8 @@ endmodule
    // assign uo_out[0] = ui_in[0] ^ ui_in[1] ^ ui_in[2];
   //  assign uo_out[1] = (ui_in[0] & ui_in[1]) |(ui_in[0] & ui_in[2]) |(ui_in[1] & ui_in[2]);
  //   assign uo_out[7:2] = 6'b0;
-//  assign uio_out = 0;
- // assign uio_oe  = 0;
+  assign uio_out = 0;
+  assign uio_oe  = 0;
 
   // List all unused inputs to prevent warnings
   wire _unused = {ena, clk, rst_n, 1'b0};
