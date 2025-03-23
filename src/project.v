6cbb1f74b72d2ae5513aaaddbfe9de3f4db2bc2c
Basil Mubarak <basilmubarak008@gmail.com>
2025-03-23 17:43:08 +0530

    Update project.v

--- src/project.v
+++ src/project.v
@@ -31,7 +31,6 @@ module tt_um_adder_multiplier(
     );
     assign uo_out[5:0] = ui_in[6]? {2'b00,adder_output}:multiplier_output;
     assign uo_out[7:6]=2'b00;
-endmodule
 
   // All output pins must be assigned. If not used, assign to 0.
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
